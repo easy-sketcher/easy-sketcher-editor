1fe96b155f0d9037ff97bfb8b9e4071f10f9ecb2
sangheon-kim <ksj83671@gmail.com>
2020-11-21 00:56:13 +0900

    [김상헌] 다이렉션 버그 수정 및 경로 수정

--- scripts/dom.ts
+++ scripts/dom.ts
@@ -4,7 +4,7 @@ let res: Rect | null;
 let startX: number, startY: number;
 function startDrag(e: MouseEvent) {
   const { pageX: x, pageY: y } = e;
-  console.log({ x, y });
+
   startX = x;
   startY = y;
   selectedElement = true;
@@ -18,31 +18,22 @@ function drag(e: MouseEvent) {
     // TODO: 디테일하게 180도 뿐만아니라 x가 작은지 y가 작은지에 따라서 처리해주기
     const { pageX: x, pageY: y } = e;
     let x1 = x - startX;
-    let y1 = y - startY;
+    let y1 = startY - y;
 
-    if (x1 < 0 && y1 < 0) {
-      // 왼쪽 위
-      x1 = Math.abs(x1);
-      y1 = Math.abs(y1);
-
-      res?.shape.setAttributeNS(
-        null,
-        "transform",
-        `rotate(180, ${res?.shape.getAttributeNS(null, "x")}, ${res?.shape.getAttributeNS(null, "y")})`
-      );
-    } else if (x1 < 0 && y1 > 0) {
-      // 왼쪽 아래
-      x1 = Math.abs(x1);
-      y1 = Math.abs(y1);
-
-      res?.shape.setAttributeNS(
-        null,
-        "transform",
-        `rotateY(180, ${res?.shape.getAttributeNS(null, "x")}, ${res?.shape.getAttributeNS(null, "y")}) `
-      );
-    } else {
-      res?.shape.setAttributeNS(null, "transform", ``);
+    if (x1 < 0 && y1 > 0) {
+      console.log("10시방향");
+      res?.shape.setAttributeNS(null, "x", `${x}`);
+      res?.shape.setAttributeNS(null, "y", `${y}`);
+    } else if (x1 < 0 && y1 < 0) {
+      console.log("7시 방향");
+      res?.shape.setAttributeNS(null, "x", `${x}`);
+    } else if (x1 > 0 && y1 > 0) {
+      console.log("2시");
+      res?.shape.setAttributeNS(null, "y", `${y}`);
     }
+
+    x1 = Math.abs(x1);
+    y1 = Math.abs(y1);
     res?.updateWidth({ width: x1, height: y1 });
   }
 }
